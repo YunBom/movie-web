d6601e9523f67a388e808528cbebc8d5cf7baffd
YunBom <ahybi986@naver.com>
2022-10-03 08:24:48 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 /* Project name : movie-web */
+/* #6.0 */
+ 
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev+1);
+  console.log("render");
   return (
     <div>
       <h1> Welcome back!!! </h1>
+      <button onClick={onClick}>Click me</button>
+      <span> Total click : {counter}</span>
     </div>
   )
 }
