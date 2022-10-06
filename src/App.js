5bdca83290c314f23b417fb28d80bf2f4a7b87ac
YunBom <ahybi986@naver.com>
2022-10-06 09:51:44 +0900

    이론 마무리

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 /* Project name : movie-web */
-/* #6.4 clean up*/
+/* #6.4 clean up */
  
 import { useState, useEffect } from "react";
 
 function Hello(){
-  function destroyFn() {
-    console.log("bye");
-  }
-  function createFn() {
-    console.log("created");
-    return destroyFn;   // 컴포넌트가 파괴될 때 createFn이 리턴한 destroyFn을 실행.
-  }
-
   useEffect(()=>{
     console.log("hi")
     return () => console.log("bye");
   })
-
-  useEffect(function(){     // 위 코드와 이 코드를 비교했을 때, 이 방법은 잘 쓰지않음.
-    console.log("hi")
-    return (function() {
-      console.log("bye!");
-    })
-  },[]);
 
   return(
     <h1>Hello!</h1>
