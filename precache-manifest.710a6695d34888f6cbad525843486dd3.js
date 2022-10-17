a3a5c3d51bb67a5a12acae6168e2cd791e735e7b
YunBom <ahybi986@naver.com>
2022-10-17 12:22:44 +0900

    Updates

---
+++ precache-manifest.710a6695d34888f6cbad525843486dd3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "958a5bfb87d438a31f1b",
+    "url": "/movie-web/static/js/main.958a5bfb.chunk.js"
+  },
+  {
+    "revision": "abac995b11ee8c07c043",
+    "url": "/movie-web/static/js/1.abac995b.chunk.js"
+  },
+  {
+    "revision": "8e6be887e09d8e1771e1",
+    "url": "/movie-web/static/js/runtime~main.8e6be887.js"
+  },
+  {
+    "revision": "6ebb47a8a3ee0ee779c19eae6c98536d",
+    "url": "/movie-web/index.html"
+  }
+];
